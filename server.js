ea883f213184acea73e9fb98a93b428fd359e4b5
samuelsantus1520-dot <samuel.santus1520@gmail.com>
2026-08-30 02:51:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { WebcastPushConnection } = require('tiktok-live-connector');
+const WebcastPushConnection = require('tiktok-live-connector');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
